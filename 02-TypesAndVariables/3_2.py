6382bfc50b64e7e0fab8451ece63030ebd06fc5b
JanKrzyzanowski <jas.krzyzanowski@gmail.com>
2025-10-23 11:12:58 +0200

    3.2

---
+++ 02-TypesAndVariables/3_2.py
@@ -0,0 +1,34 @@
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 71
+number2 = 14
+number3= 13
+result = number1 + number2 + number3
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('Number 2: ', number3)
+print('The result of summation: ', result)
+
+###
+# A program for swapping two varable values
+#
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+
+# swap the values
+...
+...
+
+print("After swapping: x=", y, "y=", x)
+
+###
+# A program that, for a given speed in km/h,
+# prints the speed in m/s
+#
+speed_kmh = 70
+speed_ms = 1
+print(speed_kmh, "km/h = ", speed_ms, "m/s")
