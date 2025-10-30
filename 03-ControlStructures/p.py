7ee57122d1053741ab2bf7284c80198ec58a5637
JanKrzyzanowski <jas.krzyzanowski@gmail.com>
2025-10-30 11:15:50 +0100

    topic 3 creations

---
+++ 03-ControlStructures/p.py
@@ -0,0 +1,16 @@
+name = "Ann"
+print(name)
+print("My name is", name)
+print(f"My name is {name}")
+
+# Get age from the user and convert to an integer
+age = int(input("Your age: "))
+
+# Check if the person is an adult
+if age > 18:
+    print("You are an adult!")
+else:
+    print("You are a child!")
+
+abc = 123
+ 
