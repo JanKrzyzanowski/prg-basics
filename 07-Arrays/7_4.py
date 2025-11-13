ba6675376028ec616da7392d2c6165ebd0a2fe2a
JanKrzyzanowski <jas.krzyzanowski@gmail.com>
2025-11-13 11:10:51 +0100

    7_7 work

---
+++ 07-Arrays/7_4.py
@@ -0,0 +1,12 @@
+grades = [2,3,7,5,4]
+print(len(grades))
+print(grades[0])
+print(grades[1])
+print(grades[4])
+print(grades[len(grades)-1])
+print(grades[0] + grades[-1])
+print[int(len(grades)/2)]
+
+for value in grades:
+    print(value, end=" ")
+
