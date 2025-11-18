ba1650d2ab19c16b3bd095518238b2453a3124c9
JanKrzyzanowski <jas.krzyzanowski@gmail.com>
2025-11-19 00:56:58 +0100

    control structures practice

---
+++ 03-ControlStructures/2_2.py
@@ -0,0 +1,12 @@
+size = input('Enter size symbol: ')
+
+if size == 'S':
+    print('S: small size')
+elif size == 'M':
+    print('M: medium size')
+elif size == 'L':
+    print('L: large size')
+elif size == 'XL':
+    print('XL: extra-large size')
+else:
+    print('You have entered an invalid input')
--- 03-ControlStructures/3_7.py
+++ 03-ControlStructures/3_7.py
@@ -4,14 +4,15 @@
 #
 basic_salary = 5000
 total_salary = 0
-bonus = 0.15 # 15%
+bonus = 0.30 # 15%
 is_bonus = input('Does the employee receive a bonus? (Y/N):') == 'Y'
 
 if is_bonus:
-    total_salary = basic_salary * bonus + basic_salary
+    total_salary = basic_salary + (basic_salary*bonus)
 else:
-    print(total_salary)
+    total_salary=basic_salary
 
 print(f'Basic salary: {basic_salary}')
-print('Bonus included? {}')
-print('Total salary: {...}')
+print(f'Bonus included? {"Yes" if is_bonus else "No"}')
+print(f'Total salary: {total_salary}')
+
