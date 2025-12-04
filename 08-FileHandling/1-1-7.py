59d212818206fca81303a94e37919898d9c06c01
JanKrzyzanowski <jas.krzyzanowski@gmail.com>
2025-12-04 14:40:48 +0100

    file handling wrok

--- 08-FileHandling/1-1-7.py
+++ 08-FileHandling/1-1-7.py
@@ -13,6 +13,6 @@ file_lines = file_content.splitlines()
 # calculates the total number of cars parked
 total = 0
 for line in file_lines:
-   total += int(line)
+   total += 
 
-print('Total cars parked:', total)
+print('Total cars parked:', ...)
